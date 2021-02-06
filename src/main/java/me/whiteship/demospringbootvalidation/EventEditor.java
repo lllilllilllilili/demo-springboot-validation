40fa1a4390f0280cc691b94341e143ffac6c28ce
lllilllilllilili <hik3562@hufs.ac.kr>
2021-02-06 23:18:58 +0900

    데이터 바인딩에 대한 실습 주석 수정

--- src/main/java/me/whiteship/demospringbootvalidation/EventEditor.java
+++ src/main/java/me/whiteship/demospringbootvalidation/EventEditor.java
@@ -15,5 +15,4 @@ public class EventEditor extends PropertyEditorSupport {
         setValue(new Event(Integer.parseInt(text)));
     }
 }
-//thread-safe 하지 않다. 프로퍼티 에디터 구현체들은 여러 스레드 공유해서 쓰면안된다. 빈으로 등록해서 쓰면 안된다.
-//프로퍼티에디터는 빈으로 등록하면 안된다. 그냥 빈이 아니라 스레드 스콥에 빈으로 만들어서 쓰면 상관없다.
+
